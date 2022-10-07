61bc8badf3cb5b61d8228101ac02f10c95b1282a
Geoff Kelly <65321097+GeoffKellyNC@users.noreply.github.com>
2022-10-06 23:04:02 -0400

    socket.io connected to front-end

--- server.js
+++ server.js
@@ -1,32 +1,42 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const http = require('http')
+
+
+const socketIo = require("socket.io");
 
 
 const app = express()
-
 app.use(express.json())
 app.use(cors())
 
+const server = http.createServer(app);
+const io = socketIo(server);
+app.set('io', io)
 
-app.use((err, req, res, next) => {
-    console.log(err.stack)
-    console.log(err.name)
-    console.log(err.code)
-
-    res.status(500).json({
-        message: 'Something went rely wrong'
-    })
+app.get('/', (req, res) => {
+    res.send('<h1>Server is running</h1>')
 })
+
+
 
 // app.use("/auth", require("./routes/authRoutes"))
 app.use("/player", require("./routes/playerRoutes"))
 app.use("/prize", require("./routes/prizeRoutes"))
 app.use("/game", require("./routes/gameRoutes"))
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    }
+    );
+  });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+
+const PORT = process.env.PORT || 9001;
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`)
 })
 
